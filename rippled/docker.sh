fd4ef78c5a191b748d11ccd7e4e7c2366520dbc7
Wazen Shbair <wazen.shbair@gmail.com>
2021-02-11 16:44:17 +0100

    Update docker.sh

--- rippled/docker.sh
+++ rippled/docker.sh
@@ -4,4 +4,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
 sudo apt install docker-ce -y
-apt install docker-compose -y
+
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
