849332847b0037b590f7a028de17f0f6c643fbbe
Wazen Shbair <wazen.shbair@gmail.com>
2021-02-15 20:25:47 +0100

    Update install.sh

--- rippled/install.sh
+++ rippled/install.sh
@@ -10,18 +10,18 @@ sudo apt -y update --allow-unauthenticated --allow-insecure-repositories
 sudo apt -y install rippled --allow-unauthenticated
 systemctl is-active rippled.service
 
-sudo apt-get install collectd -y
-sudo apt install python-pip -y
+sudo apt-get install collectd -y --allow-unauthenticated
+sudo apt install python-pip -y --allow-unauthenticated
 
-sudo apt-get install g++ curl libssl-dev -y
-sudo apt-get install git-core -y
+sudo apt-get install g++ curl libssl-dev -y --allow-unauthenticated
+sudo apt-get install git-core -y --allow-unauthenticated
 
-sudo apt install cmdtest -y
+sudo apt install cmdtest -y --allow-unauthenticated
 # Install Nodejs
 sudo apt-get install python-software-properties python make -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt install npm -y
+sudo apt-get install -y nodejs --allow-unauthenticated
+sudo apt install npm -y --allow-unauthenticated
 npm i ripple-lib
 
 sudo git clone https://github.com/etsy/statsd.git /opt/statsd
